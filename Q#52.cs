9fb15cbcbfb08cacf0163d7aaaf3f9e374196ade
Alawi-Aisha <aishaalvi433@gmail.com>
2021-11-26 11:29:35 +0500

    fisrt commit

---
+++ Q#52.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //(a)
+            int a=(1+8/2+((1*4)+(5*4))/4);
+            Console.WriteLine(a);
+            Console.ReadLine();
+
+            //b
+            int b=((1+1+1+1)/2+(1+1+1)/3);
+            Console.WriteLine(b);
+            Console.ReadLine();
+
+              //c
+            int c=(5*5+5/5+6);
+            Console.WriteLine(c);
+            Console.ReadLine();
+        
+
+            //d
+            int d=(((3+4)+(4*7))/5);
+            Console.WriteLine(d);
+            Console.ReadLine();
+
+            //e
+            int e=((3*6*7*2)/12/2);
+            Console.WriteLine(e);
+            Console.ReadLine();
+
+            //f
+            int f=(5-3*4%(6-1));
+            Console.WriteLine(f);
+            Console.ReadLine();
+
+            //g
+            int g=((8*4*2+6)/2+4);
+            Console.WriteLine(g);
+            Console.ReadLine();
+        }
+    }
+}
